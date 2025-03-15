d58a4f7f37e841271d2fe8ee7064eb2ef0c60698
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-03-15 23:34:32 +0530

    chore: token views added

--- server/userauth/models.py
+++ server/userauth/models.py
@@ -5,5 +5,4 @@ class User(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=20)
-    created_on = models.DateTimeField(auto_now_add=True)
     is_verified = models.BooleanField(default=False)
--- server/userauth/views.py
+++ server/userauth/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class HomeView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get (self, request):
+
+        content = {
+            'message': 'Welcome to the JWT Authentication page using React and Django',
+        }
+        return Response(content)
+    
+
