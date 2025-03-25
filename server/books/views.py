59de26799c224d8d68c80b3012aa53395d16c66c
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-03-25 23:25:38 +0530

    fix: issue on book_details in backend

--- server/books/views.py
+++ server/books/views.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 from os import getenv
 import requests
 
-API_URL = "https://www.googleapis.com/books/v1/volumes"
-
 def search (request):
     load_dotenv()
     query = request.GET.get('query')
@@ -13,7 +11,7 @@ def search (request):
         return JsonResponse({"error": "Query parameter is required"}, status=400)
     
     api_key = getenv("BOOKS_API_KEY")
-    url = "{API_URL}?q={query}&key={api_key}"
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}"
 
     response = requests.get(url)
     print(response)
@@ -24,10 +22,11 @@ def search (request):
     else:
         return JsonResponse({"error": "Failed to get data from Google Books"}, status=500)
 
-def get_book_details (request, book_id):
-    response = requests.get(f"{API_URL}{book_id}")
+def get_book_details (book_id):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={book_id}")
     if response.status_code == 200:
-        return response
+        print(response)
+        return JsonResponse(response.json())
     else :
         JsonResponse({"error": "Book not found"}, status=404)
 
