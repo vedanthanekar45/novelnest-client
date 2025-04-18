03eda37ca075ebb0f5c8c37998f31fb9175d6be6
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-04-18 17:22:27 +0530

    feat: added book_logging functionality api

--- server/books/urls.py
+++ server/books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.search, name="search")
+    path('search/', views.search, name="search"),
+    path('log_book/', views.log_book, name="log_book")
 ]
--- server/books/views.py
+++ server/books/views.py
@@ -1,7 +1,10 @@
 from django.http import JsonResponse
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from dotenv import load_dotenv
 from os import getenv
 import requests
+from .models import bookLogData
 
 def search (request):
     load_dotenv()
@@ -20,4 +23,25 @@ def search (request):
         return JsonResponse(data)
     else:
         return JsonResponse({"error": "Failed to get data from Google Books"}, status=500)
-    
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def log_book (request):
+    data = request.data
+    created = bookLogData.objects.get_or_create(
+        user = request.user,
+        book_id = data['google_book_id'],
+        defaults = {
+            'title' : data['title'],
+            'thumbnail_url' : data['thumbnail_url'],
+            'status': data['status'],
+            'rating': data.get('rating'),
+            'notes': data.get('notes')
+        }
+    )
+
+    if not created:
+        return JsonResponse({'message': 'Already logged this book'}, status=400)
+
+    return JsonResponse({'message': 'Book logged successfully!'})
