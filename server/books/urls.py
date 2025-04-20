9194edaf6f6355ded751c2b4f1ac0559a2db66cf
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-04-20 13:08:46 +0530

    feat: shelf feature in backend, need some fixes

--- server/books/urls.py
+++ server/books/urls.py
@@ -2,9 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    # search url path
     path('search/', views.search, name="search"),
+
+    # book data related paths
     path('log_book/', views.log_book, name="log_book"),
     path('get_user_books/', views.get_user_books, name="get_user_books"),
     path('count_book_logs/', views.count_book_logs, name="count_book_logs"),
     path('get_book_data/', views.get_book_details, name="get_book_details"),
+
+    # user shelf related paths
+    path('create_shelf/', views.create_shelf, name="create_shelf"),
+    path('add_book_to_shelf/', views.add_book_to_shelf, name="add_book_to_shelf"),
 ]
--- server/books/views.py
+++ server/books/views.py
@@ -4,15 +4,14 @@ from rest_framework.decorators import api_view, permission_classes
 from dotenv import load_dotenv
 from os import getenv
 import requests
-from .models import bookLogData
+from .models import *
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
-
+#  ------- SEARCH APIS --------------
 def search (request):
-
     load_dotenv()
     query = request.GET.get('query')
 
@@ -37,12 +36,14 @@ def search (request):
 
 
 
+
+# --------- BOOK DATA RELATED APIS ------------
+
 # View to create a log
 @api_view(['POST'])
 # IsAuthenticated() ensures the data goes in only when the user is logged in
 @permission_classes([IsAuthenticated])
 def log_book (request):
-
     data = request.data
 
     # I've used get_or_create in order to not create a duplicate entry of the log
@@ -71,7 +72,6 @@ def log_book (request):
 # Get the books that the user has logged to their profile
 @api_view(['GET'])
 def get_user_books (request, username):
-
     user = User.objects.get(username=username)
     logs = bookLogData.objects.filter(user=user).order_by('logged_time')
 
@@ -85,7 +85,6 @@ def get_user_books (request, username):
 @api_view(['GET'])
 # Count how many times the book has been logged for each category
 def count_book_logs (request):
-
     # Getting the book id
     book_id = request.GET.get('id')
 
@@ -103,7 +102,6 @@ def count_book_logs (request):
 
 @api_view(['GET'])
 def get_book_details (request):
-
     load_dotenv()
 
     id = request.GET.get('id')
@@ -120,3 +118,44 @@ def get_book_details (request):
         return JsonResponse({
             "error": "Failed to get data from Google Books"
         }, status=500)
+    
+
+
+# --------- USER SHELVES RELATED APIS -----------
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_shelf (request):
+    data = request.data
+    shelf_title = data['title']
+    shelf_description = data['description']
+
+    shelf, created = userShelf.objects.get_or_create(user=request.user, shelfTitle=shelf_title, description=shelf_description)
+
+    return JsonResponse({
+        'message': "Shelf created successfully."
+    }, status=200)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_book_to_shelf (request):
+    load_dotenv()
+    id = request.GET.get('id')
+    api_key = getenv("BOOKS_API_KEY")
+    url = f"https://www.googleapis.com/books/v1/volumes/{id}?key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+
+    book_id = data['id']
+    title = data['volumeInfo']['title']
+    thumbnail = data['volumeInfo'].get('imageLinks', {}).get('thumbnail', '')
+
+    shelf_id = userShelf.objects.get(id)
+    shelfbook = ShelfBook.objects.create(shelf=shelf_id, book_id=book_id, title=title, 
+                                         thumbnail_url=thumbnail)
+    shelfbook.save()
+
+    return JsonResponse({
+        'message': "Book successfully added to shelf."
+    }, status=200)
